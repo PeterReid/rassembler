552369adbbeb61ec0f27c2893af8b105a36885f0
Peter Reid <peter.d.reid@gmail.com>
2016-09-25 12:57:49 -0400

    rassembler constructs an Arch enum

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,15 @@ extern crate elf_writer;
 mod object_file;
 pub mod x64;
 
+pub enum Arch {
+    X64(x64::Assembler)
+}
+
+#[cfg(target_arch = "x86_64")]
+pub fn new(object_name: &str) -> Arch {
+    Arch::X64(x64::Assembler::new())
+}
+
 #[test]
 fn it_works() {
 }
